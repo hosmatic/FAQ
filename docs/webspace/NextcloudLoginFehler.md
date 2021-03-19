5e4bde111ac466a827024fe92fd0af998a1df6a9
MG_100 <32128537+MG-100@users.noreply.github.com>
2021-03-19 22:26:08 +0100

    add nextcloud

---
+++ docs/webspace/NextcloudLoginFehler.md
@@ -0,0 +1,10 @@
+---
+id: NextcloudLoginFehler
+title: Nextcloud Login Fehler beheben
+---
+
+> Schritt 1
+
+Erstelle einen Ordner namens phpsessions auserhalb des Nextcloud Ordner
+
+> Fertig!
--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     Generell: ['general/infos'],
     Account: ['account/Credits', 'account/Partner', 'account/DiscordVerifizieren', 'account/AusversehenProduktBestellt'],
-    Webspace: ['webspace/ConnectDomain', 'webspace/ZertifikatErstellen', 'webspace/Webmail'],
+    Webspace: ['webspace/ConnectDomain', 'webspace/ZertifikatErstellen', 'webspace/Webmail', 'webspace/NextcloudLoginFehler'],
     Gameserver: ['gameserver/GameServerStartetNicht'],
     MusicBots: ['musicbot/musicsoon'],
   },
