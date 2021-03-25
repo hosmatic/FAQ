31604f876a4c2666ba8b50fd5266180e832ef2a1
--replace-all <admin@mg100.net>
2021-03-25 10:26:34 +0100

    change sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,34 @@
 module.exports = {
-  someSidebar: {
-    Generell: ['general/infos'],
-    Account: ['account/Credits', 'account/Partner', 'account/DiscordVerifizieren', 'account/AusversehenProduktBestellt'],
-    Webspace: ['webspace/ConnectDomain', 'webspace/ZertifikatErstellen', 'webspace/Webmail', 'webspace/NextcloudLoginFehler', 'webspace/gitverbinden'],
-    Gameserver: ['gameserver/GameServerStartetNicht'],
-    MusicBots: ['musicbot/musicsoon'],
-    Datenbanken: ['database/mysql'],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'Generell',
+      items: ['general/infos'],
+    },
+    {
+      type: 'category',
+      label: 'Account',
+      items: ['account/Credits', 'account/Partner', 'account/DiscordVerifizieren', 'account/AusversehenProduktBestellt'],
+    },
+    {
+      type: 'category',
+      label: 'Webspace',
+      items: ['webspace/ConnectDomain', 'webspace/ZertifikatErstellen', 'webspace/Webmail', 'webspace/NextcloudLoginFehler', 'webspace/gitverbinden'],
+    },
+    {
+      type: 'category',
+      label: 'Gameserver',
+      items: ['gameserver/GameServerStartetNicht'],
+    },
+    {
+      type: 'category',
+      label: 'MusicBots',
+      items: ['musicbot/musicsoon'],
+    },
+    {
+      type: 'category',
+      label: 'Datenbanken',
+      items: ['database/mysql'],
+    },
+  ],
 };
