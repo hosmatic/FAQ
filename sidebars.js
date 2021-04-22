266cd944b7c9d01d0ba8fcfaef171d00d05519ca
DerKartdomi <krogdomi@gmail.com>
2021-04-22 12:16:16 +0200

    updated sidebar

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
       {
         type: 'category',
         label: 'Generell',
-        items: ['general/infos', 'general/beratung'],
+        items: ['general/infos', 'general/Beratung'],
       },
       {
         type: 'category',
